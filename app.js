8678b0569e1213b94a44c010e7d179727c9d714b
rimonspi12 <rimonmpi@gmail.com>
2021-08-18 13:52:04 +0600

    phone price add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // function for calcuated
 
-// recive input value and calculated price
+// recive input value and calculated back pad price
 
 function getInputValue(product, isTrue, price) {
     const productInput = document.getElementById(product + '-number');
@@ -12,15 +12,36 @@ function getInputValue(product, isTrue, price) {
     } else if (productNumber > 0) {
         productNumber = parseInt(productNumber) - 1;
     }
+
     // assingn the value of productInput
     productInput.value = productNumber;
-    // assingn the value of totalPrice;
-    priceInput.innerText = productNumber * price;
+
+    //update case total
+
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+
 
 }
 
+// handle phone price
+
+document.getElementById('phone-plus').addEventListener('click', function () {
+    getInputValue('phone', true, 1219);
+    // const phoneInput = document.getElementById('phone-number');
+    // const phoneNumber = parseInt(phoneInput.value);
+    // console.log(phoneNumber);
+    // // update phone number in display;
+    // phoneInput.value = phoneNumber + 1;
+});
 
 
+document.getElementById('phone-minus').addEventListener('click', function () {
+    getInputValue('phone', false, 1219);
+});
+
+
+// handle phone phone back pad
 document.getElementById('case-plus').addEventListener('click', function () {
     getInputValue('case', true, 59)
 })
@@ -28,3 +49,4 @@ document.getElementById('case-plus').addEventListener('click', function () {
 document.getElementById('case-minus').addEventListener('click', function () {
     getInputValue('case', false, 59);
 })
+
